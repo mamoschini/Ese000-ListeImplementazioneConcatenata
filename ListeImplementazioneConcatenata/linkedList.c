9c7f7ac3a4f098afe7f70b14901698bcc0b77246
mtmoschini <matteo_moschini@libero.it>
2015-01-28 12:43:46 +0100

    PrimaCommit

--- ListeImplementazioneConcatenata/linkedList.c
+++ ListeImplementazioneConcatenata/linkedList.c
@@ -6,8 +6,14 @@
  * Returns the updated pointer to the first element of the list.
  */
 LLElement * LLInsertAtBeginning(LLElement * first, int key) {
-    // TODO To be implemented
-    return NULL;
+    LLElement * new;
+    new = (LLElement *)malloc(sizeof(LLElement));
+    if(new != NULL) {
+        new->next = first;
+        new->key = key;
+        first = new;
+    }
+    return first;
 }
 
 /*
@@ -15,8 +21,18 @@ LLElement * LLInsertAtBeginning(LLElement * first, int key) {
  * Returns the updated pointer to the first element of the list.
  */
 LLElement * LLInsertAtEnd(LLElement * first, int key) {
-    // TODO To be implemented
-    return NULL;
+    LLElement * new;
+    LLElement ** temp;
+    new = (LLElement *)malloc(sizeof(LLElement));
+    if(new != NULL) {
+        temp = &first;
+        while(*temp != NULL)
+            temp = &((*temp)->next);
+        new->next = NULL;
+        new->key = key;
+        *temp = new;
+    }
+    return first;
 }
 
 /*
@@ -27,24 +43,42 @@ LLElement * LLInsertAtEnd(LLElement * first, int key) {
  * Returns the updated pointer to the first element of the list.
  */
 LLElement * LLInsertAtPosition(LLElement * first, int key, int position) {
-    // TODO To be implemented
-    return NULL;
+    LLElement * new;
+    LLElement ** temp;
+    int i;
+    new = (LLElement *)malloc(sizeof(LLElement));
+    if(new != NULL) {
+        temp = &first;
+        for(i=0; i<position; i++)
+            temp = &((*temp)->next);
+        new->next = *temp;
+        new->key = key;
+        *temp = new;
+    }
+    return first;
 }
 
 /*
  * Returns the size of the list.
  */
 int LLSize(LLElement * first) {
-    // TODO To be implemented
-    return 0;
+    int r = 0;
+    while(first != NULL) {
+        r++;
+        first = first->next;
+    }
+    return r;
 }   
 
 /*
  * Returns the key at the specified position.
  */ 
 int LLGetKey(LLElement * first, int position) {
-    // TODO To be implemented
-    return 0;
+    int i;
+    for(i=0; i<position; i++) {
+        first = first->next;
+    }
+    return first->key;
 }
 
 /*
@@ -53,8 +87,19 @@ int LLGetKey(LLElement * first, int position) {
  * Returns -1 if not found. 
  */ 
 int LLFindKey(LLElement * first, int key, int startPosition) {
-    // TODO To be implemented
-    return -1;
+    int r = -1;
+    int i;
+    for(i=0; i<startPosition; i++) {
+        first = first->next;
+    }
+    while(first != NULL && r == -1) {
+        if(first->key == key) {
+            r = i;
+        }
+        first = first->next;
+        i++;
+    }
+    return r;
 }
 
 /*
@@ -62,8 +107,13 @@ int LLFindKey(LLElement * first, int key, int startPosition) {
  * Returns the updated pointer to the first element of the list.
  */
 LLElement * LLRemoveFirst(LLElement * first) {
-    // TODO To be implemented
-    return NULL;
+    LLElement * temp;
+    if(first != NULL) {
+        temp = first;
+        first = first->next;
+        free(temp);
+    }
+    return first;
 }
 
 /*
@@ -71,8 +121,16 @@ LLElement * LLRemoveFirst(LLElement * first) {
  * Returns the updated pointer to the first element of the list.
  */
 LLElement * LLRemoveLast(LLElement * first) {
-    // TODO To be implemented
-    return NULL;
+    LLElement **temp;
+    if(first != NULL) {
+        temp = &first;
+        while((*temp)->next != NULL) {
+                temp = &((*temp)->next);
+            }
+        free(*temp);
+        *temp = NULL;
+    }
+    return first;
 }
 
 /*
@@ -83,8 +141,21 @@ LLElement * LLRemoveLast(LLElement * first) {
  * Returns the updated pointer to the first element of the list.
  */
 LLElement * LLRemoveAtPosition(LLElement * first, int position) {
-    // TODO To be implemented
-    return NULL;
+    LLElement ** temp;
+    LLElement * pDelete;
+    int i;
+    
+    temp = &first;
+    for(i=0; i<position; i++)
+        temp = &((*temp)->next);
+    
+    pDelete = *temp;
+    if((*temp)->next != NULL) {
+        *temp = (*temp)->next;
+    }
+    free(pDelete);
+    
+    return first;
 }
 
 
--- ListeImplementazioneConcatenata/nbproject/Makefile-variables.mk
+++ ListeImplementazioneConcatenata/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=None-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
 CND_ARTIFACT_NAME_Debug=listeimplementazioneconcatenata
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/listeimplementazioneconcatenata
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/listeimplementazioneconcatenata
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
 CND_PACKAGE_NAME_Debug=listeimplementazioneconcatenata.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/listeimplementazioneconcatenata.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/listeimplementazioneconcatenata.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=None-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
 CND_ARTIFACT_NAME_Release=listeimplementazioneconcatenata
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/listeimplementazioneconcatenata
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/listeimplementazioneconcatenata
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
 CND_PACKAGE_NAME_Release=listeimplementazioneconcatenata.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/listeimplementazioneconcatenata.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/listeimplementazioneconcatenata.tar
 #
 # include compiler specific variables
 #
